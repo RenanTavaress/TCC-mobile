77c5fbd9daf2994eacb422d85a52914868ca694d
Renan Tavares <renan_velox@hotmail.com>
2022-10-04 15:53:28 -0300

    estilizando o a tela de ongScreen

--- src/screens/ScreenUser/OngScreen/index.tsx
+++ src/screens/ScreenUser/OngScreen/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, HeaderOng, Description, City, District } from "./styles";
 import { Header } from "../../../components/Header";
 import { useRoute } from "@react-navigation/native";
 
 export function OngScreen() {
-	const {params} = useRoute() as { params: { id: string, nameOng: string } };;
+	const {params} = useRoute() as { params: { id: string, nameOng: string, descricao: string, city: string, district: string } };;
 
-	const { nameOng } = params 
+	const { nameOng, descricao, city, district } = params 
 	
 	return (
 		<Container>
 			<Header title={nameOng} icon="left" />
+			<HeaderOng>
+				<Description>{descricao}</Description>
+				<City>{city}</City>	
+				<District>{district}</District>
+
+			</HeaderOng>
 		</Container>
 	);
 }
+
+
--- src/screens/ScreenUser/OngScreen/styles.ts
+++ src/screens/ScreenUser/OngScreen/styles.ts
@@ -3,3 +3,15 @@ import styled from "styled-components/native";
 export const Container = styled.View`
 	flex: 1;
 `;
+
+export const HeaderOng = styled.View`
+	background-color: ${({theme}) => theme.colors.shape};
+	
+
+`
+
+export const Description = styled.Text``
+
+export const City = styled.Text`` 
+
+export const District = styled.Text``
